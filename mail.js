c4044754ba57a91926f6cd35a74639f7ce5e49dc
Diep <diepmv@rikkeisoft.com>
2017-12-02 11:44:26 +0700

    first commit

---
+++ mail.js
@@ -0,0 +1,26 @@
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+	service: "gmail",
+	auth: {
+	user: 'maidiep2610@gmail.com',
+	pass: 'maithituyet'
+	}
+
+});
+
+var mailOptions = {
+	from: 'maidiep2610@gmail.com',
+	to: ["maidiep.ptit@gmail.com", "maidiep2610@gmail.com", "huynt124@gmail.com"],
+	subject: "Sending an Email using Node.js",
+	text: "That was easy!"
+
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+	if (error){
+		console.log(error);
+	} else {
+		console.log("Email sent: " + info.response);
+	}
+
+});
